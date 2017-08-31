92be144be71b08f6ed54f1c8f9e903a09799157c
Samuel Duzett <samuel.duzett@gmail.com>
2017-08-31 14:55:40 -0600

    display long path to phantom

--- index.js
+++ index.js
@@ -12,13 +12,27 @@ exports.handler = function(event, context, callback) {
 			files.forEach(file => {
 				console.log('./node_modules/' + file);
 			});
-			fs.readdir('./node_modules/html-pdf', (err, files) => {
+			fs.readdir('./node_modules/phantomjs-prebuilt', (err, files) => {
 				files.forEach(file => {
-					console.log('./node_modules/html-pdf/' + file);
+					console.log('./node_modules/phantomjs-prebuilt/' + file);
 				});
-				generator.test();
+				fs.readdir('./node_modules/phantomjs-prebuilt/lib', (err, files) => {
+					files.forEach(file => {
+						console.log('./node_modules/phantomjs-prebuilt/lib/' + file);
+					});
+					gfs.readdir('./node_modules/phantomjs-prebuilt/phantom', (err, files) => {
+						files.forEach(file => {
+							console.log('./node_modules/phantomjs-prebuilt/phantom/' + file);
+						});
+						gfs.readdir('./node_modules/phantomjs-prebuilt/phantom/bin', (err, files) => {
+							files.forEach(file => {
+								console.log('./node_modules/phantomjs-prebuilt/phantom/bin' + file);
+							});
+							generator.test();
+						});
+					});
+				});
 			});
-			generator.test();
 		});
 	});
 };
