3f0197fbe2113934e6e18aac9c5e74897c30ad5c
Samuel Duzett <samuel.duzett@gmail.com>
2017-08-31 12:15:57 -0600

    blind stab in dark

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ var AWS = require('aws-sdk'),
 	generator = require('./generator.js');
 
 exports.handler = function(event, context, callback) {
-	generator.test();
+	const fs = require('fs');
+
+	fs.readdir('./', (err, files) => {
+		files.forEach(file => {
+			console.log(file);
+		});
+		generator.test();
+	});
 };
