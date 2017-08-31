bc963cd57b85708b721aca1cf7199dd5a13379f1
Samuel Duzett <samuel.duzett@gmail.com>
2017-08-31 15:54:51 -0600

    remove phantomPath

--- index.js
+++ index.js
@@ -3,39 +3,42 @@ var AWS = require('aws-sdk'),
 
 exports.handler = function(event, context, callback) {
 	const fs = require('fs');
+	generator.test();
 
-	fs.readdir('./', (err, files) => {
-		files.forEach(file => {
-			console.log(file);
-		});
-		fs.readdir('./node_modules', (err, files) => {
-			files.forEach(file => {
-				console.log('./node_modules/' + file);
-			});
-			fs.readdir('./node_modules/phantomjs-prebuilt', (err, files) => {
-				files.forEach(file => {
-					console.log('./node_modules/phantomjs-prebuilt/' + file);
-				});
-				fs.readdir('./node_modules/phantomjs-prebuilt/lib', (err, files) => {
-					if (err) return console.error(err);
-					files.forEach(file => {
-						console.log('./node_modules/phantomjs-prebuilt/lib/' + file);
-					});
-					fs.readdir('./node_modules/phantomjs-prebuilt/lib/phantom', (err, files) => {
-						if (err) return console.error(err);
-						files.forEach(file => {
-							console.log('./node_modules/phantomjs-prebuilt/lib/phantom/' + file);
-						});
-						fs.readdir('./node_modules/phantomjs-prebuilt/lib/phantom/bin', (err, files) => {
-							if (err) return console.error(err);
-							files.forEach(file => {
-								console.log('./node_modules/phantomjs-prebuilt/lib/phantom/bin' + file);
-							});
-							generator.test();
-						});
-					});
-				});
-			});
-		});
-	});
+	// fs.readdir('./', (err, files) => {
+	// 	files.forEach(file => {
+	// 		console.log(file);
+	// 	});
+	// 	fs.readdir('./node_modules', (err, files) => {
+	// 		files.forEach(file => {
+	// 			console.log('./node_modules/' + file);
+	// 		});
+	// 		fs.readdir('./node_modules/phantomjs-prebuilt', (err, files) => {
+	// 			files.forEach(file => {
+	// 				console.log('./node_modules/phantomjs-prebuilt/' + file);
+	// 			});
+	// 			fs.readdir('./node_modules/phantomjs-prebuilt/lib', (err, files) => {
+	// 				if (err) return console.error(err);
+	// 				files.forEach(file => {
+	// 					console.log('./node_modules/phantomjs-prebuilt/lib/' + file);
+	// 				});
+	// 				fs.readdir('./node_modules/phantomjs-prebuilt/lib/phantom', (err, files) => {
+	// 					if (err) return console.error(err);
+	// 					else{
+	// 						files.forEach(file => {
+	// 							console.log('./node_modules/phantomjs-prebuilt/lib/phantom/' + file);
+	// 						});
+	// 						fs.readdir('./node_modules/phantomjs-prebuilt/lib/phantom/bin', (err, files) => {
+	// 							if (err) return console.error(err);
+	// 							files.forEach(file => {
+	// 								console.log('./node_modules/phantomjs-prebuilt/lib/phantom/bin' + file);
+	// 							});
+	// 							generator.test();
+	// 						});
+	// 					}
+	// 				});
+	// 			});
+	// 		});
+	// 	});
+	// });
 };
